c9ec8bdd821000365d7e07e3ce3cd35ebe453a48
Loren Hicks <lorenhicks@gmail.com>
2025-08-05 23:45:16 -0400

    Verify API Key

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY"))
 
 course_content = """
 An operating system (OS) is system software that manages computer hardware, software resources, 
